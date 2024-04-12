6a9a9817699fe91755effad9719669a2b29b9023
Isoken Ibizugbe <isokenjune@gmail.com>
2024-04-12 03:27:26 +0000

    task 1

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,6 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+    const rooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(24)];
+    return rooms;
+}
